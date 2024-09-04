bef4e46a7799015931272eeb30e2836503a0dcc9
Andrey <andreyharapetyan521@gmail.com>
2024-09-04 21:19:37 +0400

    First Homework

---
+++ Homework5.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+import java.util.Scanner;
+
+public class Homework5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter first integer");
+        int firstinteger = scanner.nextInt();
+
+        System.out.println("Enter second integer");
+        int secondinteger = scanner.nextInt();
+
+        if (firstinteger % secondinteger == 0) {
+            System.out.println("True");
+        } else {
+            System.out.println("False");
+        }
+
+    }
+}
