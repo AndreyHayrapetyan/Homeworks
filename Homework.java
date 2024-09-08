8f736c8a1c4bf7de6e5385a14cb5e05a765011a5
Andrey <andreyharapetyan521@gmail.com>
2024-09-08 19:42:02 +0400

    Homework

--- Homework.java
+++ Homework.java
@@ -1,11 +1,16 @@
-//Homework
 import java.util.Scanner;
-public class Homework{
+public class Homework {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Enter a digit");
+        System.out.println("Enter a digit ");
         int number = scanner.nextInt();
+
+        if (number > 999 && number < 10000 ) {
+            processData(number);
+        } else {
+            System.exit(0);
+        }
 
         System.out.println(number);
         System.out.println(number / 1000);
@@ -13,5 +18,10 @@ public class Homework{
         System.out.println(number % 100 / 10);
         System.out.println(number % 10);
 
+
+    }
+
+    public static void processData(int number) {
+        System.out.println("Processing data " + number);
     }
 }
