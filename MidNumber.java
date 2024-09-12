3ffd978499b29025a953b6dbd3b4948dfba80438
Andrey <andreyhayrapetyan521@gmail.com>
2024-09-12 23:11:01 +0400

    Homework

---
+++ MidNumber.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MidNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter number between 1 and 8 ");
+        int n = scanner.nextInt();
+        System.out.println("Enter the first number ");
+        int a = scanner.nextInt();
+        System.out.println("Enter the second number ");
+        int b = scanner.nextInt();
+
+        if (n <= 0 || n > 8) {
+            System.out.println("The number should be between 1 and 8");
+            System.exit(1);
+        }
+
+        int[] array = new int[n];
+
+        int mid = n / 2;
+        array[mid - 1] = a;
+        array[mid + 1] = b;
+
+
+
+        System.out.println(Arrays.toString(array));
+
+
+    }
+}
