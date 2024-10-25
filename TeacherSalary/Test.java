c45c8385c463f9e1dcfa7a1c08371b0fd3178c12
Andrey <andreyhayrapetyan521@gmail.com>
2024-10-25 14:37:56 +0400

    Homework

--- TeacherSalary/Test.java
+++ TeacherSalary/Test.java
@@ -5,9 +5,9 @@ public class Test {
         Teacher.setK(1.5);
         Teacher.setMinSalary(56000);
 
-        Teacher teacher1 = new Teacher("Alice", "Math",  2);
+        Teacher teacher1 = new Teacher("Tom", "Math",  2);
         Teacher teacher2 = new Teacher("Bob", "Physics",  1.1);
-        Teacher teacher3 = new Teacher("Carol", "Chemistry",  0.8);
+        Teacher teacher3 = new Teacher("Cathrine", "History",  0.8);
 
         System.out.println("Teacher 1 (" + teacher1.getName() + ") Salary: " + teacher1.getSalary());
         System.out.println("Teacher 2 (" + teacher2.getName() + ") Salary: " + teacher2.getSalary());
