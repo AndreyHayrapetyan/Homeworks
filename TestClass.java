460d13f9e57c7b71c12797c152120fcc74e73568
AndreyHayrapetyan <164780348+AndreyHayrapetyan@users.noreply.github.com>
2025-05-26 15:53:11 +0400

    Update TestClass.java

--- TestClass.java
+++ TestClass.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebDriver;
 
 import java.io.IOException;
 import java.util.*;
-
+@Tag("UI")
 @ExtendWith(ScreenshotOnFailureExtension.class)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class TestClass {
